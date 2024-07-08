3f4b7e161779d044d5e8aa47b96b810dac9fcb0e
Seine <>
2024-07-09 01:40:22 +0900

    20240709 more schema

--- src/db.js
+++ src/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetube");
-// , { useNewUrlParser: true, useUnifiedTopology: true, }
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", { 
+    //useNewUrlParser: true, useUnifiedTopology: true, 
+    useFindAndModify: false,
+});
 
 const db = mongoose.connection;
 
